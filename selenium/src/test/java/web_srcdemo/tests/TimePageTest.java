0d2aa739568bec25a7ab4d8f3cd063c66e07577f
Nimit Tanboontor <6531503042@lamduan.mfu.ac.th>
2024-10-29 16:07:24 +0700

    Create TimePageTest.java

---
+++ selenium/src/test/java/web_srcdemo/tests/TimePageTest.java
@@ -0,0 +1,22 @@
+//// TimePageTest.java
+//package web_srcdemo.tests;
+//
+//import org.testng.annotations.Test;
+//import web_srcdemo.pages.TimePage;
+//
+//public class TimePageTest extends BaseTest {
+//
+//    @Test
+//    public void testTimePageActions() {
+//        TimePage timePage = new TimePage(driver);
+//
+//        // Navigate to Time menu
+//        timePage.navigateToTimeMenu();
+//
+//        // Select employee by typing a partial name and view timesheet
+//        timePage.selectEmployeeHintAndViewTimesheet("Ranga");
+//
+//        // Click create timesheet if available
+//        timePage.createTimesheetIfAvailable();
+//    }
+//}
